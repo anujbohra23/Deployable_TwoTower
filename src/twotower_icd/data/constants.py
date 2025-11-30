f0747091df82044839b2c15b1e6b1f1121ac83d7
Anuj Bohra <anujbohra10@gmail.com>
2025-11-29 23:12:15 -0500

    Not using pdfplumber for now, will revise it later

---
+++ src/twotower_icd/data/constants.py
@@ -0,0 +1,22 @@
+'''
+Defines which lab features we expect in patients_scaled.csv if there are additions
+or deletions they need to be updated here.
+'''
+
+LAB_KEYS = [
+    "a1c",
+    "glucose",
+    "creatinine",
+    "egfr",
+    "ldl",
+    "hdl",
+    "triglycerides",
+    "wbc",
+    "hgb",
+    "platelets",
+    "crp",
+    "troponin",
+    "bnp",
+    "alt",
+    "ast",
+]
